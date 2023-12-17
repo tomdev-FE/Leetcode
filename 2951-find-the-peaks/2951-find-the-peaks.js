bdb999ce072095321c82437a55c4cae6798f5b55
tomdev-FE <114313432+tomdev-FE@users.noreply.github.com>
2023-12-17 18:57:46 +0700

    Time: 65 ms (25.14%), Space: 43.6 MB (88.52%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} mountain
+ * @return {number[]}
+ */
+var findPeaks = function(mountain) {
+    let result = [];
+    
+    for(let i = 1;i<mountain.length-1;i++){
+        if(mountain[i] > mountain[i+1] && mountain[i] > mountain[i-1]){
+            result.push(i)
+        }
+    }
+    return result
+};
