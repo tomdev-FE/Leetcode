ea2b5073d7c386c8787c10b9cb5c1c23aceac24a
tomdev-FE <114313432+tomdev-FE@users.noreply.github.com>
2023-12-17 18:09:32 +0700

    Time: 58 ms (15.73%), Space: 41.4 MB (94.43%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    if(nums.length === 1){
+        return 0
+    }
+    for(let i = 0;i<nums.length;i++){
+        if(i===0&& nums[i]>nums[i+1]){
+            return i
+        } else if(i === nums.length - 1 && nums[i]>nums[i-1]){
+            return i
+        }
+        else if (nums[i] > nums[i-1] && nums[i] > nums[i+1]){
+            return i
+        }
+    }
+};
