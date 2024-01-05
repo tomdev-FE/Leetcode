406adc6cdef0495f79cd4d0d71b10d581945b61d
tomdev-FE <114313432+tomdev-FE@users.noreply.github.com>
2024-01-05 15:14:26 +0700

    Time: 62 ms (46.24%), Space: 46.4 MB (23.03%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+  let start = 0;
+  let min = Infinity;
+  let sum = 0;
+  
+    for(let end=0;end<nums.length;end++){
+        sum += nums[end];
+        while(sum >=target){
+            min = Math.min(min,end-start+1);
+            sum -= nums[start];
+            start++
+        }
+    }
+    return min !== Infinity ? min : 0 
+};
