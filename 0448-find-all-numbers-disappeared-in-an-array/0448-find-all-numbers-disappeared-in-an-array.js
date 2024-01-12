f8fcc2e9a183cffff18667050595c2a340c5614e
tomdev-FE <114313432+tomdev-FE@users.noreply.github.com>
2024-01-12 16:01:34 +0700

    Time: 84 ms (75.14%), Space: 53.1 MB (31.96%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -3,9 +3,20 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
- const n = nums.length;
-  const allNums = Array.from({length: n}, (_, i) => i + 1);
-  const inputNums = new Set(nums);
-  
-  return allNums.filter(x => !inputNums.has(x));
+    let i =0;
+    let missedNumbers = []
+    while(i<nums.length){
+        const j = nums[i] -1;
+        if(nums[i]!==nums[j]){
+            [nums[i],nums[j]]=[nums[j],nums[i]]
+        } else{
+            i++
+        }
+    }
+    for(let i =0;i<nums.length;i++){
+        if(nums[i]!==(i+1)){
+            missedNumbers.push(i+1)
+        }
+    }
+    return missedNumbers
 };
