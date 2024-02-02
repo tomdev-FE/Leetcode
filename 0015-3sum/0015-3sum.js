42005a9e18c4e8fbe81b87bfdc4a6bd96790ebd8
tomdev-FE <114313432+tomdev-FE@users.noreply.github.com>
2024-02-02 15:56:26 +0700

    Time: 159 ms (53.56%), Space: 66 MB (14.38%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -2,38 +2,42 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
- function  searchPair(arr, targetSum, left, triplets) {
-    let right = arr.length - 1;
-    while (left < right) {
-      const currentSum = arr[left] + arr[right];
-      if (currentSum === targetSum) { // found the triplet
-        triplets.push([-targetSum, arr[left], arr[right]]);
-        left += 1;
-        right -= 1;
-        while (left < right && arr[left] === arr[left - 1]) {
-          left += 1; // skip same element to avoid duplicate triplets
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    const n = nums.length;
+
+    for (let k = 0; k < n - 2; k++) {
+        if (k > 0 && nums[k] === nums[k - 1]) {
+            continue; // Skip duplicate values of k
         }
-        while (left < right && arr[right] === arr[right + 1]) {
-          right -= 1; // skip same element to avoid duplicate triplets
+
+        let left = k + 1;
+        let right = n - 1;
+
+        while (left < right) {
+            const sum = nums[k] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[k], nums[left], nums[right]]);
+
+                // Skip duplicates for left and right
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++;
+                }
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--;
+                }
+
+                left++;
+                right--;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                left++;
+            }
         }
-      } else if (targetSum > currentSum) {
-        left += 1; // we need a pair with a bigger sum
-      } else {
-        right -= 1; // we need a pair with a smaller sum
-      }
-    }
-  }
-var threeSum = function(arr) {
-    arr.sort((a, b) => a - b);
-    const triplets = [];
-    for (let i = 0; i < arr.length; i++) {
-      // skip same element to avoid duplicate triplets
-      if (i > 0 && arr[i] === arr[i - 1]) { 
-        continue;
-      }
-     searchPair(arr, -arr[i], i + 1, triplets);
     }
 
-    return triplets;
-    
+    return result;
 };
