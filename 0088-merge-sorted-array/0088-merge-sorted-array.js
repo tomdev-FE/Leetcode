2b7578a26c2fed97b31201e800e33291cda19424
tomdev-FE <114313432+tomdev-FE@users.noreply.github.com>
2024-03-02 13:09:11 +0700

    Time: 54 ms (53.84%), Space: 49.2 MB (16.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,18 +6,14 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    while (n > 0) {
-    //if val in nums1 is greater than val in nums2, use nums1
-    if (nums1[m - 1] > nums2[n - 1]) {
-      nums1[m + n - 1] = nums1[m - 1];
-      m--;
-    }
-    //otherwise use nums2 val
-    else {
-      nums1[m + n - 1] = nums2[n - 1];
-      n--;
-    }
-  }
-            
-   
+   while(n>0){
+       if(nums1[m-1]>nums2[n-1]){
+           nums1[m+n-1]=nums1[m-1];
+           m--
+       } else {
+           nums1[m+n-1]=nums2[n-1];
+           n--
+       }
+   }
+    return nums1
 };
